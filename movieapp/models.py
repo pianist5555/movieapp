9f4d71f33ea82fbe93eec1e5c7d5727446bee73c
Ubuntu <ubuntu@ip-172-31-41-193.ap-northeast-2.compute.internal>
2021-12-08 07:24:27 +0000

    movie rating became null allowed in movie (migrate)

--- movieapp/models.py
+++ movieapp/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     year = models.CharField(max_length=50) 
     genres = models.CharField(max_length=50) 
     summary = models.CharField(max_length=50)
-    rating = models.DecimalField(max_digits = 3, decimal_places = 1)
+    rating = models.DecimalField(max_digits = 3, decimal_places = 1, null=True, blank=True)
     
 class MovieReview(models.Model):
     movie = models.ForeignKey(Movie, models.PROTECT)
