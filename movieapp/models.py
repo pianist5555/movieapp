b537b6e0604a6fb776f668cdc95671bae1b29611
Ubuntu <ubuntu@ip-172-31-41-193.ap-northeast-2.compute.internal>
2021-12-08 07:59:09 +0000

    summary column became null allowed in moviereview (migrate)

--- movieapp/models.py
+++ movieapp/models.py
@@ -14,7 +14,7 @@ class MovieReview(models.Model):
     text = models.CharField(max_length=50)
     rating = models.DecimalField(max_digits = 3, decimal_places = 1) 
     created_at = models.DateTimeField(null=True, blank=True)
-    summary = models.CharField(max_length=50) 
+    summary = models.CharField(max_length=50, null=True, blank=True) 
     is_deleted = models.BooleanField(default=False)
 
 class MovieReviewVote(models.Model):
