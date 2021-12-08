d8c96a61b1063e12ce2fbec784ca1b596e041165
Ubuntu <ubuntu@ip-172-31-41-193.ap-northeast-2.compute.internal>
2021-12-08 05:23:34 +0000

    url connected

---
+++ movieapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from movieapp import views
+
+urlpatterns_movie = [
+    path('list', views.movieapp_list, name='api_movie_list'),
+    path('detail', views.movieapp_detail, name='api_movie_detail'),
+    path('create', views.movieapp_create, name='api_movie_create'),
+]
+urlpatterns_review = [
+    path('detail', views.review_detail, name='api_review_detail'),
+    path('create', views.review_create, name='api_review_create'),
+    path('update', views.review_update, name='api_review_update'),
+    path('delete', views.review_delete, name='api_review_delete'),
+]
+urlpatterns_vote = [
+    path('list', views.vote_create, name='api_vote_create'),
+    path('detail', views.vote_delete, name='api_vote_delete'),
+]
--- movieapp/views.py
+++ movieapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+def movieapp_list(request):
+    return HttpResponse(json.dumps({'test':"movieapp_list url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def movieapp_detail(request):
+    return HttpResponse(json.dumps({'test':"movieapp_detail url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def movieapp_create(request):
+    return HttpResponse(json.dumps({'test':"movieapp_create url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def review_detail(request):
+    return HttpResponse(json.dumps({'test':"review_detail url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def review_create(request):
+    return HttpResponse(json.dumps({'test':"review_create url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def review_update(request):
+    return HttpResponse(json.dumps({'test':"review_update url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def review_delete(request):
+    return HttpResponse(json.dumps({'test':"review_delete url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def vote_create(request):
+    return HttpResponse(json.dumps({'test':"vote_create url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+def vote_delete(request):
+    return HttpResponse(json.dumps({'test':"vote_delete url connect succeed"}),
+                            content_type='application/json; charset=utf8')
+
+
+
